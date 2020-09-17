558df22feabbdd89a448e45379def7a3f5e13dc3
Christine Kim <chrismekim@gmail.com>
2020-09-17 10:55:36 -0500

    In progress

--- src/js/header.js
+++ src/js/header.js
@@ -1,22 +1,28 @@
+import seaborneLogo from '../img/seaborne-logo.svg';
+
 export const header = () => {
-  let wrapper = document.createElement('header');
+  //To-do: Update all links. Add logic to check if environment is not production, then use '.html'
+
+  const wrapper = document.createElement('header');
+  wrapper.classList =
+    'header site-padding-x inner-content-max-width mxauto flex justify-between items-center flex-row pt1';
 
   wrapper.innerHTML = `
-  <header
-  class="header site-padding-x inner-content-max-width mxauto flex justify-between items-center flex-row pt1"
-  >
-  <a aria-label="Visit Seaborne" href="/index.html" class="button--style-no-style">
-    <img class="fit-contain w100" src="img/seaborne-logo.svg" alt="Seaborne logo" />
-  </a>
-  <a
-    aria-label="Get in touch with Seaborne"
-    href="/contact.html"
-    class="button--style-blue-border text-body-sm"
-  >
-    Get in touch
-  </a>
-  </header>
+    <a aria-label="Visit Seaborne" href="/index.html" class="button--style-no-style">
+      <img class="seaborne-logo fit-contain w100" src="img/seaborne-logo.svg" alt="Seaborne logo" />
+    </a>
+    <a
+      aria-label="Get in touch with Seaborne"
+      href="/contact.html"
+      class="button--style-blue-border text-body-sm"
+    >
+      Get in touch
+    </a>
   `;
+
+  window.addEventListener('load', function () {
+    document.getElementsByClassName('seaborne-logo')[0].src = seaborneLogo;
+  });
 
   return wrapper;
 };
