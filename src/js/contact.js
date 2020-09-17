39cbed61302dd12a6e4c9803648c65e3709f0582
Christine Kim <chrismekim@gmail.com>
2020-09-17 13:50:34 -0500

    Add copy to clipboard function

--- src/js/contact.js
+++ src/js/contact.js
@@ -5,3 +5,20 @@ const contact = document.getElementById('contact');
 contact.prepend(header());
 contact.appendChild(footer());
 document.body.appendChild(contact);
+
+//Adds Copy to Clipboard functionality
+const copyToClipBoard = (string) => {
+  const copyElement = document.createElement('textarea');
+  copyElement.value = string;
+  document.body.appendChild(copyElement);
+  copyElement.select();
+  document.execCommand('copy');
+  document.body.removeChild(copyElement);
+};
+
+const copyButton = document.getElementsByClassName('copy-button')[0];
+
+copyButton.addEventListener('click', function () {
+  const copyText = document.getElementsByClassName('email-address')[0].textContent;
+  copyToClipBoard(copyText);
+});
