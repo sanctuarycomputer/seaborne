587fa82ac40f7b1ea250e6e7b3e257a4e6dfc43b
Christine Kim <chrismekim@gmail.com>
2020-09-17 15:32:03 -0500

    Add transitions

--- src/js/script.js
+++ src/js/script.js
@@ -12,7 +12,25 @@ const copyToClipBoard = (string) => {
 
 const copyButton = document.getElementsByClassName('copy-button')[0];
 
-copyButton.addEventListener('click', function () {
-  const copyText = document.getElementsByClassName('email-address')[0].textContent;
-  copyToClipBoard(copyText);
-});
+if (copyButton) {
+  copyButton.addEventListener('click', function () {
+    const copyText = document.getElementsByClassName('email-address')[0].textContent;
+    copyToClipBoard(copyText);
+  });
+}
+
+// Add transition when section enters viewport
+const sections = document.querySelectorAll('section.fade-in');
+
+window.onscroll = function () {
+  if (!document.querySelectorAll('section.fade-in:not(.visible)')) return;
+
+  for (const section of sections) {
+    if (
+      section.getBoundingClientRect().top <= window.innerHeight * 0.75 &&
+      section.getBoundingClientRect().top > 0
+    ) {
+      section.classList.add('visible');
+    }
+  }
+};
