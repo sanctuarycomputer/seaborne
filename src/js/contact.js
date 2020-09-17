060eaf6e7fb1320db7e4343ac85f6c70869cb455
Christine Kim <chrismekim@gmail.com>
2020-09-17 15:10:39 -0500

    Add posthtml-include

--- src/js/contact.js
+++
@@ -1,7 +0,0 @@
-import { header } from './header';
-import { footer } from './footer';
-
-const contact = document.getElementById('contact');
-contact.prepend(header());
-contact.appendChild(footer());
-document.body.appendChild(contact);
