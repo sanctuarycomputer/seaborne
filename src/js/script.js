feb0a1b3718853bbfb545c302497fbc8d5caf65c
Christine <43737723+chrismekim@users.noreply.github.com>
2020-09-21 08:28:16 -0500

    Add Contact Section and Copy to Clipboard Button Functionality (#21)
    * modularize footer and header
    
    * In progress
    
    * Fix merge conflicts
    
    * In progress
    
    * Add contact page
    
    * Add alt
    
    * delete weird node module
    
    * Fix console error
    
    * Add posthtml-include
    
    * Add contact section and copy to clipboard functionality
    
    * add site padding x
    
    * adjust padding

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
-"use strict";
+'use strict';
+
+//Copy to Clipboard functionality
+const copyToClipBoard = (string) => {
+  const copyElement = document.createElement('textarea');
+  copyElement.value = string;
+  document.body.appendChild(copyElement);
+  copyElement.select();
+  document.execCommand('copy');
+  document.body.removeChild(copyElement);
+};
+
+const copyButton = document.getElementsByClassName('copy-button')[0];
+
+copyButton.addEventListener('click', function () {
+  const copyText = document.getElementsByClassName('email-address')[0].textContent;
+  copyToClipBoard(copyText);
+});
