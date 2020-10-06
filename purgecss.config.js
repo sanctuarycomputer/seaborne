50de0c07decb8b18ff6377119ebb093b9629d241
Christine Kim <chrismekim@gmail.com>
2020-10-06 12:10:30 -0500

    Clean up

--- purgecss.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  content: ['src/**/*.html'],
-  whitelist: ['opacity-1', 'opacity-0', 'events-none', 'none', 'events-all', 'visible'],
-  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:]+/g) || [],
-};
